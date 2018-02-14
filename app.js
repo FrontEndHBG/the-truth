92884c8d27ecc367283d82dfe67242f884ccc740
Max Carlquist <tenkakle@gmail.com>
2018-02-14 15:44:28 +0100

    first and last commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var names = ['Max', 'Dimitrij', 'Calle'];
+
+
+for (var i = 0; i < names.length; i++) {
+    if(names[i] === 'Max') {
+        document.getElementById('h1').innerText = "Yeah he's a cool guy!";
+    }
+}
